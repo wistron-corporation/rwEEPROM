005e2f6606f50206cf48f9fc2b2b0efe0dc42670
AndyYFWang <Andy_YF_Wang@wistron.com>
2020-05-06 17:22:32 +0800

    Fix the format of the file

--- wEE.hpp
+++ wEE.hpp
@@ -8,23 +8,25 @@
 #define mac0_addr 0x87
 #define mac1_addr 0x100
 
-using namespace std; 
+using namespace std;
 
-int fd;                                    // eeprom binary file.
-void openBinFile();                        // Open the binary file of eeprom.
-void rwPrettyName(int addr);               // Write & read "PrettyName" to the binary file of eeprom.
-void rwPartNumber(int addr);               // Write & read "PartNumber" to the binary file of eeprom.
-void rwSerialNumber(int addr);             // Write & read "SerialNumber" to the binary file of eeprom.
-void rwMAC(int addr, string data);         // Write & read "MAC0" to the binary file of eeprom.
-string hexToASCII(string hex);             // Hex-to-ASCII for string.
-void paserXML();                           // Paser the xml file.
+int fd;             // eeprom binary file.
+void openBinFile(); // Open the binary file of eeprom.
+void rwPrettyName(
+    int addr); // Write & read "PrettyName" to the binary file of eeprom.
+void rwPartNumber(
+    int addr); // Write & read "PartNumber" to the binary file of eeprom.
+void rwSerialNumber(
+    int addr); // Write & read "SerialNumber" to the binary file of eeprom.
+void rwMAC(int addr,
+           string data); // Write & read "MAC0" to the binary file of eeprom.
+string hexToASCII(string hex); // Hex-to-ASCII for string.
+void paserXML();               // Paser the xml file.
 
-
-string vpdMAC0;       // The VPD_Date of MAC0.
-string vpdMAC1;       // The VPD_Date of MAC1.
-string vpdPN;         // The VPD_Date of PartNumber.
-string vpdSN;         // The VPD_Date of SerialNumber.
-string vpdPTN;        // The VPD_Date of PrettyName.
-
+string vpdMAC0; // The VPD_Date of MAC0.
+string vpdMAC1; // The VPD_Date of MAC1.
+string vpdPN;   // The VPD_Date of PartNumber.
+string vpdSN;   // The VPD_Date of SerialNumber.
+string vpdPTN;  // The VPD_Date of PrettyName.
 
 #endif
